b14b789c7602377fff76253cc952d50377debea9
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-04-19 22:03:03 +0530

    BasicStructure

---
+++ coding-prep/Basic C++/BasicStructure.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+struct book
+{
+    int bookid;
+    char title[20];
+    float price;
+};
+void display(book);
+book input();
+book input()
+{
+    book b;
+    cout << "Enter BookId, Title and Price of the Book :";
+    cin >> b.bookid >> b.title >> b.price;
+    return b;
+}
+void display(book b)
+{
+    cout << endl
+         << b.bookid << " " << b.title << " " << b.price;
+}
+void main()
+{
+    book b1;
+    b1 = input();
+    display(b1);
+}
