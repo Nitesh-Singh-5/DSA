7d41dc0e5583d8994e02ff8186f67f10d262b43f
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-04-23 22:29:14 +0530

    edited friend function program

--- coding-prep/Basic C++/10. FriendFn.cpp
+++ coding-prep/Basic C++/10. FriendFn.cpp
@@ -10,19 +10,7 @@ public:
     void getdata();
     friend int add();
 };
-void Sum ::getdata()
-{
-    cout << "Enter Two number :" << endl;
-    cin >> a >> b;
-}
-int add()
-{
-    Sum s;
-    int temp;
-    s.getdata();
-    temp = s.a + s.b; // accessing a and b i.e private data with friend function
-    return temp;
-}
+
 int main()
 {
     int result = add();
