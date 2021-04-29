4d6a75b217f627b11ab0a7f35641ed42bd3e36e7
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-04-29 21:53:52 +0530

    edited operator_overloading.cpp

--- coding-prep/Basic C++/15. Operator_overl.cpp
+++ coding-prep/Basic C++/15. Operator_overl.cpp
@@ -21,15 +21,3 @@ public:
         return c;
     }
 };
-
-int main()
-{
-    Complex c1, c2, c3;
-    cout << "Enter value of a:" << endl;
-    c1.get_data();
-    cout << "Enter value of b:" << endl;
-    c2.get_data();
-    c3 = c1 + c2; //     c1.add(c2)
-    c3.show_data();
-    return 0;
-}
