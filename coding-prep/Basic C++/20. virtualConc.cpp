a059794884e7d72800e34e07f47ab3cdc6dc1cae
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-05-01 21:25:36 +0530

    edited vitral base concept

--- coding-prep/Basic C++/20. virtualConc.cpp
+++ coding-prep/Basic C++/20. virtualConc.cpp
@@ -8,22 +8,3 @@ public:
         cout << "f1 function of class A defined";
     }
 };
-class B : public A
-{
-public:
-    void f1() // function overriding
-    {
-        cout << "f1 function of class B defined";
-    }
-    void f2()
-    {
-    }
-};
-int main()
-{
-    A *p, o1;
-    B o2;
-    p = &o2;
-    p->f1(); //B
-    return 0;
-}
