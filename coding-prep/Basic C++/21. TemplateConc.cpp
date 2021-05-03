6c077e17e2c097bbb5a465371f70309047b2141b
Nitesh Singh <ns98738698@gmail.com>
2021-05-03 22:42:49 +0530

    Update 21. TemplateConc.cpp

--- coding-prep/Basic C++/21. TemplateConc.cpp
+++ coding-prep/Basic C++/21. TemplateConc.cpp
@@ -1,18 +1,1 @@
-#include <iostream>
-using namespace std;
 
-template <class X>
-X big(X a, X b)
-{
-    if (a > b)
-    {
-        return a;
-    }
-    else
-        return (b);
-}
-int main()
-{
-    cout << big(4, 5) << endl;
-    cout << big(3.5, 7.6) << endl;
-}
