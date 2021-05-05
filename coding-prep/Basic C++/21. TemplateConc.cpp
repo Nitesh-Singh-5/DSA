233409f02df03e6c5a7c6a3cce9b8b0899574434
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-05-05 23:24:57 +0530

    deleted template.cpp

---
+++ coding-prep/Basic C++/21. TemplateConc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+template <class X>
+X big(X a, X b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    else
+        return (b);
+}
+int main()
+{
+    cout << big(4, 5) << endl;
+    cout << big(3.5, 7.6) << endl;
+}
