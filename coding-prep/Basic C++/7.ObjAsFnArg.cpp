7af9bf58538d537c54ab25b0ef72a88c358e8676
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-05-29 15:48:08 +0530

    added Object as Function argument

---
+++ coding-prep/Basic C++/7.ObjAsFnArg.cpp
@@ -0,0 +1,36 @@
+// Objects as FUnction Arguments pass by value
+
+#include <iostream>
+using namespace std;
+class Total
+{
+    int n;
+
+public:
+    void getdata();
+    void putdata();
+    void add(Total, Total);
+};
+void Total ::getdata()
+{
+    cout << "Enter a number :" << endl;
+    cin >> n;
+}
+void Total ::putdata()
+{
+    cout << n << endl;
+}
+void Total ::add(Total x, Total y)
+{
+    n = x.n + y.n;
+}
+int main()
+{
+    Total a, b, result;
+    a.getdata();
+    b.getdata();
+    result.add(a, b);
+    cout << "Sum of the numbers :" << endl;
+    result.putdata();
+    return 0;
+}
