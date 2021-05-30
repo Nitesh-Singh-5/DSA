034ca1fdf53c1c8fdcd5d63dd036660ce5ae7760
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-05-30 12:17:47 +0530

    added types of recursion

--- coding-prep/DSA/Recursion/TypesOfRecursion.cpp
+++ coding-prep/DSA/Recursion/TypesOfRecursion.cpp
@@ -35,3 +35,63 @@ int main()
     fun(x);
     return 0;
 }
+
+//Tree Recursion
+
+#include <stdio.h>
+void fun(int n)
+{
+    if (n > 0)
+    {
+        printf("%d ", n);
+        fun(n - 1);
+        fun(n - 1);
+    }
+}
+int main()
+{
+    fun(3);
+    return 0;
+}
+
+// Indirect Recursion
+#include <stdio.h>
+void funB(int n);
+void funA(int n)
+{
+    if (n > 0)
+    {
+        printf("%d ", n);
+        funB(n - 1);
+    }
+}
+void funB(int n)
+{
+    if (n > 1)
+    {
+        printf("%d ", n);
+        funA(n / 2);
+    }
+}
+int main()
+{
+    funA(20);
+    return 0;
+}
+
+//Nested Recursion
+
+#include <stdio.h>
+int fun(int n)
+{
+    if (n > 100)
+        return n - 10;
+    return fun(fun(n + 11));
+}
+int main()
+{
+    int r;
+    r = fun(95);
+    printf("%d\n", r);
+    return 0;
+}
