1d2a354629b5ba7c62fbb0ce2d43e9681470badd
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-05-31 23:36:15 +0530

    added Applications of recursion

---
+++ coding-prep/DSA/Recursion/Factorial of N.cpp
@@ -0,0 +1,25 @@
+// Factorial of N
+
+#include <iostream>
+using namespace std;
+
+int fact(int n)
+{
+    if (n == 0)
+        return 1;
+    return fact(n - 1) * n;
+}
+int Ifact(int n)
+{
+    int f = 1, i;
+    for (i = 1; i <= n; i++)
+        f = f * i;
+
+    return f;
+}
+int main()
+{
+    int r = Ifact(5);
+    cout << r;
+    return 0;
+}
