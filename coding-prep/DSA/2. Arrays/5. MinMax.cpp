358b783f75100aabda2f7ec19c5690f39e58fc0e
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-06-19 23:25:29 +0530

    added Minimum amd Maximum of a array

---
+++ coding-prep/DSA/2. Arrays/5. MinMax.cpp
@@ -0,0 +1,64 @@
+// Minimum element
+
+#include <iostream>
+using namespace std;
+int findSmallestElement(int arr[], int n)
+{
+    int temp = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (temp > arr[i])
+        {
+            temp = arr[i];
+        }
+    }
+    return temp;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    int arr[n - 1];
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int smallest = findSmallestElement(arr, n);
+    cout << "Smallest Element is: " << smallest;
+    return 0;
+}
+
+// Maximum Element
+
+#include <iostream>
+using namespace std;
+int findaximumElement(int arr[], int n)
+{
+    int temp = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (temp < arr[i])
+        {
+            temp = arr[i];
+        }
+    }
+    return temp;
+}
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    int arr[n - 1];
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int Maximum = findaximumElement(arr, n);
+    cout << "Maximum Element is: " << Maximum;
+    return 0;
+}
